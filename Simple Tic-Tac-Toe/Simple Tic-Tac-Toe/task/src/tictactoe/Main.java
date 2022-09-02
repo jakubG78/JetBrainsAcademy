0b8f085294a703294c9baab2d96602b44b7efa0f
jakubG78 <jakub.gora@wp.pl>
2022-09-02 12:56:04 +0200

    commit first stage tic-tac-toe

--- Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -2,6 +2,16 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        char[][] playBoard = {
+                {'X', 'X', 'O' },
+                {'O', 'O', 'O' },
+                {'O', 'O', 'O' }};
+
+        for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < 3; columnIndex++) {
+                System.out.print(playBoard[rowIndex][columnIndex] + " ");
+            }
+            System.out.println();
+        }
     }
 }
