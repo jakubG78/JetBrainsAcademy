d9c90413851b640643e1e9570f4fc291b4f7d96c
jakubG78 <jakub.gora@wp.pl>
2022-09-07 18:53:20 +0200

    commit final stage tic-tac-toe

--- Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -3,38 +3,52 @@ package tictactoe;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static java.lang.Math.abs;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char[][] playBoard = new char[3][3];
-        String input = scanner.nextLine();
-        int charIndex = 0;
+
+        // initial variables values
+
         for (int rowIndex = 0; rowIndex < 3; rowIndex++) {
             for (int columnIndex = 0; columnIndex < 3; columnIndex++) {
-                playBoard[rowIndex][columnIndex] = input.charAt(charIndex);
-                charIndex++;
+                playBoard[rowIndex][columnIndex] = ' ';
             }
         }
-        printBoard(playBoard);
+        String whoIsWinner = "D";
+        char whoPlays = 'X';
 
-/*        if (Impossible(input)) {
-            System.out.println("Impossible");
-        } else if (XWin(input)) {
-            System.out.println("X wins");
-        } else if (OWin(input)) {
-            System.out.println("O wins");
-        } else if (GameNotFinished(input)) {
-            System.out.println("Game not finished");
-        } else {
-            System.out.println("Draw");
-        }*/
-        move(scanner, playBoard);
         printBoard(playBoard);
+        while (gameNotFinished(playBoard)) {
+            move(scanner, playBoard, whoPlays);
+            if (whoPlays == 'X') {
+                whoPlays = 'O';
+            } else {
+                whoPlays = 'X';
+            }
+            printBoard(playBoard);
+            if (checkWinner(playBoard, 'X')) {
+                whoIsWinner = "X";
+                break;
+            } else if (checkWinner(playBoard, 'O')) {
+                whoIsWinner = "O";
+                break;
+            }
+        }
+        switch (whoIsWinner) {
+            case "X":
+                System.out.println("X wins");
+                break;
+            case "O":
+                System.out.println("O wins");
+                break;
+            default:
+                System.out.println("Draw.");
+                break;
+        }
     }
 
-    private static void move(Scanner scanner, char[][] playBoard) {
+    private static void move(Scanner scanner, char[][] playBoard, char whoPlays) {
         int xMove;
         int yMove;
         while (true) {
@@ -50,10 +64,10 @@ public class Main {
             }
 
             if (xMove <= 3 && yMove <= 3) {
-                if (playBoard[xMove - 1][yMove - 1] != '_') {
+                if (playBoard[xMove - 1][yMove - 1] != ' ') {
                     System.out.println("This cell is occupied! Choose another one!");
                 } else {
-                    playBoard[xMove - 1][yMove - 1] = 'X';
+                    playBoard[xMove - 1][yMove - 1] = whoPlays;
                     break;
                 }
             } else {
@@ -78,40 +92,25 @@ public class Main {
         System.out.println("---------");
     }
 
-    public static boolean GameNotFinished(String input) {
-        return input.contains("_");
-    }
-
-    public static boolean XWin(String input) {
-        return input.substring(3, 6).equals("XXX") | input.startsWith("XXX") | input.endsWith("XXX")
-                | (input.charAt(0) == 'X' && input.charAt(4) == 'X' && input.charAt(8) == 'X')
-                | (input.charAt(0) == 'X' && input.charAt(3) == 'X' && input.charAt(6) == 'X')
-                | (input.charAt(1) == 'X' && input.charAt(4) == 'X' && input.charAt(7) == 'X')
-                | (input.charAt(2) == 'X' && input.charAt(5) == 'X' && input.charAt(8) == 'X')
-                | (input.charAt(2) == 'X' && input.charAt(4) == 'X' && input.charAt(6) == 'X');
-    }
-
-    public static boolean OWin(String input) {
-        return input.substring(3, 6).equals("OOO") | input.startsWith("OOO") | input.endsWith("OOO")
-                | (input.charAt(0) == 'O' && input.charAt(4) == 'O' && input.charAt(8) == 'O')
-                | (input.charAt(0) == 'O' && input.charAt(3) == 'O' && input.charAt(6) == 'O')
-                | (input.charAt(1) == 'O' && input.charAt(4) == 'O' && input.charAt(7) == 'O')
-                | (input.charAt(2) == 'O' && input.charAt(5) == 'O' && input.charAt(8) == 'O')
-                | (input.charAt(2) == 'O' && input.charAt(4) == 'O' && input.charAt(6) == 'O');
-    }
-
-    public static boolean Impossible(String input) {
-        int Xs = 0;
-        int Os = 0;
-        for (int i = 0; i < input.length(); ++i) {
-            if (input.charAt(i) == 'X') {
-                ++Xs;
-            } else if (input.charAt(i) == 'O') {
-                ++Os;
+    public static boolean gameNotFinished(char[][] playBoard) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (playBoard[i][j] == ' ') return true;
             }
         }
-        return abs(Xs - Os) > 1 || (OWin(input) && XWin(input));
+        return false;
     }
 
+    public static boolean checkWinner(char[][] playBoard, char toBeChecked) {
+        return (playBoard[0][0] == toBeChecked && playBoard[0][1] == toBeChecked && playBoard[0][2] == toBeChecked)
+                | (playBoard[1][0] == toBeChecked && playBoard[1][1] == toBeChecked && playBoard[1][2] == toBeChecked)
+                | (playBoard[2][0] == toBeChecked && playBoard[2][1] == toBeChecked && playBoard[2][2] == toBeChecked)
+                | (playBoard[0][0] == toBeChecked && playBoard[1][1] == toBeChecked && playBoard[2][2] == toBeChecked)
+                | (playBoard[0][2] == toBeChecked && playBoard[1][1] == toBeChecked && playBoard[2][0] == toBeChecked)
+                | (playBoard[1][0] == toBeChecked && playBoard[1][1] == toBeChecked && playBoard[1][2] == toBeChecked)
+                | (playBoard[0][0] == toBeChecked && playBoard[1][0] == toBeChecked && playBoard[2][0] == toBeChecked)
+                | (playBoard[0][1] == toBeChecked && playBoard[1][1] == toBeChecked && playBoard[2][1] == toBeChecked)
+                | (playBoard[0][2] == toBeChecked && playBoard[1][2] == toBeChecked && playBoard[2][2] == toBeChecked);
+    }
 }
 
