d4b4f5b7108c615e9c78cae4ecb2205c89fbe9af
jakubG78 <jakub.gora@wp.pl>
2022-09-10 20:24:52 +0200

    commit for CinemaRoomManager stage 5/5

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -8,41 +8,61 @@ public class Cinema {
 
         int premiumSeatsPrice = 10;
         int normalSeatsPrice = 8;
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int rows = scanner.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        int columns = scanner.nextInt();
-        char[][] seatsSetup = new char[columns][rows];
-
-        createCinemaRoom(seatsSetup);
+        char[][] seatsSetup = createCinemaRoom(scanner);
         menuInterface(premiumSeatsPrice, normalSeatsPrice, seatsSetup, scanner);
 
     }
 
     private static void buyTicket(int premiumSeatsPrice, int normalSeatsPrice, Scanner scanner, char[][] seatsSetup) {
-        System.out.println("Enter a row number:");
-        int chosenRowNumber = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int chosenColumnNumber = scanner.nextInt();
-        seatsSetup[chosenColumnNumber - 1][chosenRowNumber - 1] = 'B';
-        System.out.printf("\nTicket price: $%d\n", checkTicketPrice(premiumSeatsPrice, normalSeatsPrice, seatsSetup, chosenRowNumber));
+        while (true) {
+            System.out.println("\n" +
+                    "Enter a row number:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Wrong input!");
+                scanner.next();
+            }
+            int chosenRowNumber = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Wrong input!");
+                scanner.next();
+            }
+            int chosenColumnNumber = scanner.nextInt();
+            if (chosenRowNumber > 0 && chosenColumnNumber <= seatsSetup.length
+                    && chosenColumnNumber > 0 && chosenRowNumber <= seatsSetup[0].length) {
+                if (seatsSetup[chosenColumnNumber - 1][chosenRowNumber - 1] == 'B') {
+                    System.out.println("That ticket has already been purchased!\n");
+                } else {
+                    seatsSetup[chosenColumnNumber - 1][chosenRowNumber - 1] = 'B';
+                    System.out.printf("\nTicket price: $%d \n", checkTicketPrice(premiumSeatsPrice, normalSeatsPrice, seatsSetup, chosenRowNumber));
+                    break;
+                }
+            } else {
+                System.out.println("Wrong input!\n");
+            }
+        }
     }
 
-    private static void createCinemaRoom(char[][] seatsSetup) {
+    private static char[][] createCinemaRoom(Scanner scanner) {
+        System.out.println("Enter the number of rows:");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int columns = scanner.nextInt();
+        char[][] seatsSetup = new char[columns][rows];
         for (int rowIndex = 0; rowIndex < seatsSetup[0].length; rowIndex++) {
             for (int columnIndex = 0; columnIndex < seatsSetup.length; columnIndex++) {
                 seatsSetup[columnIndex][rowIndex] = 'S';
             }
         }
+        return seatsSetup;
     }
 
     private static int checkTicketPrice(int premiumSeatsPrice, int normalSeatsPrice, char[][] seatsSetup, int chosenRowNumber) {
         int ticketPrice;
         if (seatsSetup.length * seatsSetup[0].length < 60) {
             ticketPrice = premiumSeatsPrice;
-        } else if (chosenRowNumber <= (seatsSetup[0].length / 2)) {
+        } else if (chosenRowNumber <= seatsSetup[0].length / 2) {
             ticketPrice = premiumSeatsPrice;
         } else {
             ticketPrice = normalSeatsPrice;
@@ -64,14 +84,41 @@ public class Cinema {
             }
             System.out.println();
         }
-        System.out.println();
+    }
+
+    private static void statistics(int premiumSeatsPrice, int normalSeatsPrice, char[][] seatsSetup) {
+        int soldTicketsCounter = 0;
+        int totalIncome = 0;
+        int currentIncome = 0;
+        for (int rowIndex = 0; rowIndex < seatsSetup[0].length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < seatsSetup.length; columnIndex++) {
+                totalIncome += checkTicketPrice(premiumSeatsPrice, normalSeatsPrice, seatsSetup, rowIndex + 1);
+                if (seatsSetup[columnIndex][rowIndex] == 'B') {
+                    currentIncome += checkTicketPrice(premiumSeatsPrice, normalSeatsPrice, seatsSetup, rowIndex + 1);
+                    soldTicketsCounter++;
+                }
+            }
+        }
+        double percentage;
+        if (seatsSetup.length * seatsSetup[0].length != 0) {
+            percentage = (double) soldTicketsCounter * 100 / (seatsSetup.length * seatsSetup[0].length);
+        } else {
+            percentage = 0;
+        }
+
+        System.out.printf("%nNumber of purchased tickets: %d\n", soldTicketsCounter);
+        System.out.printf("Percentage: %.2f%%%n", percentage);
+        System.out.printf("Current income: $%d%n", currentIncome);
+        System.out.printf("Total income: $%d%n", totalIncome);
     }
 
     private static void menuInterface(int premiumSeatsPrice, int normalSeatsPrice, char[][] seatsSetup, Scanner scanner) {
         boolean isNotOver = true;
         while (isNotOver) {
-            System.out.println("1. Show the seats");
+            System.out.println("\n" +
+                    "1. Show the seats");
             System.out.println("2. Buy a ticket");
+            System.out.println("3. Statistics");
             System.out.println("0. Exit");
 
             int inputCommand = scanner.nextInt();
@@ -82,6 +129,9 @@ public class Cinema {
                     break;
                 case 2:
                     buyTicket(premiumSeatsPrice, normalSeatsPrice, scanner, seatsSetup);
+                    break;
+                case 3:
+                    statistics(premiumSeatsPrice, normalSeatsPrice, seatsSetup);
                     break;
                 case 0:
                     isNotOver = false;
