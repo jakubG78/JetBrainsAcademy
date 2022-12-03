1fe51c42c7d61ece56da6c4c408ea659cecf3690
jakubG78 <jakub.gora@wp.pl>
2022-12-03 23:55:41 +0100

    03.12.2022 commit

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int secretCodeLength = Integer.valueOf(scanner.nextLine());
+        long pseudoRandomNumber = System.nanoTime();
+        
+    }
+
+    private static void userInterface(Scanner scanner) {
         String secretCode = "9305";
         int cowsCounter = 0;
         int bullsCounter = 0;
