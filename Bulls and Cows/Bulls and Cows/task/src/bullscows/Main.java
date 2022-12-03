43902608703d38937a417d76af89fa82052e6690
jakubG78 <jakub.gora@wp.pl>
2022-12-03 16:45:47 +0100

    Bulls and Cows stage 1 complete commit

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -2,6 +2,13 @@ package bullscows;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("The secret code is prepared: ****.");
+
+        for(int i = 0; i<2; i++){
+            System.out.println("Turn 1. Answer:\n" +
+                    "1234\n" +
+                    "Grade: 1 cow.\n");
+        }
+        System.out.println("Congrats! The secret code is 9305.");
     }
 }
