da701033c4f73ef3f534818e6b2dc80d939171b4
jakubG78 <jakub.gora@wp.pl>
2022-12-05 15:15:24 +0100

    Bulls and Cows stage 3 complete commit

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -1,17 +1,44 @@
 package bullscows;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int secretCodeLength = Integer.valueOf(scanner.nextLine());
-        long pseudoRandomNumber = System.nanoTime();
-        
+        userInterface(scanner);
+    }
+
+    private static String generateCode(int secretCodeLength) {
+        Long pseudoRandomNumber = 0L;
+        String numbersPool = "";
+        String generatedCode = "";
+        ArrayList<Character> codeNumbers = new ArrayList<>();
+        if (secretCodeLength < 10) {
+            do {
+                pseudoRandomNumber = System.nanoTime();
+                numbersPool = pseudoRandomNumber.toString();
+                for (int i = 0; i < secretCodeLength; i++) {
+                    if (!codeNumbers.contains(numbersPool.charAt(i))) {
+                        codeNumbers.add(numbersPool.charAt(i));
+                    }
+                }
+            } while (codeNumbers.size() < secretCodeLength);
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Character singleNumber : codeNumbers) {
+                stringBuilder.append(singleNumber);
+            }
+            generatedCode = stringBuilder.toString();
+
+        } else {
+            System.out.println("Error - secret code, too long.");
+        }
+        return generatedCode;
     }
 
     private static void userInterface(Scanner scanner) {
-        String secretCode = "9305";
+        int secretCodeLength = Integer.valueOf(scanner.nextLine());
+        String secretCode = generateCode(secretCodeLength);
         int cowsCounter = 0;
         int bullsCounter = 0;
         char[] playerInput = scanner.nextLine().toCharArray();
