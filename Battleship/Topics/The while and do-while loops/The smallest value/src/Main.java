a616aaa234651e8c7a3b38395eb3ae5328557a60
jakubG78 <jakub.gora@wp.pl>
2022-12-11 22:53:12 +0100

    11.12.2022 commit

--- Battleship/Topics/The while and do-while loops/The smallest value/src/Main.java
+++ Battleship/Topics/The while and do-while loops/The smallest value/src/Main.java
@@ -3,6 +3,24 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        long inputLong = scanner.nextLong();
+        long tempFactValue = 1;
+        int searchedNumber = 1;
+        while (true) {
+            tempFactValue = factorial(searchedNumber);
+            if (!(tempFactValue > inputLong)) {
+                searchedNumber++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(searchedNumber);
+    }
+
+    public static long factorial(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return n * factorial(n - 1);
     }
 }
