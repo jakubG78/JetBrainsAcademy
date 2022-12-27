645bebb2d717bd47cfd2e1b96fd68cc70f240546
jakubG78 <jakub.gora@wp.pl>
2022-12-27 22:35:10 +0100

    27.12.2022 commit

--- Number Base Converter/Number Base Converter/task/src/converter/Main.java
+++ Number Base Converter/Number Base Converter/task/src/converter/Main.java
@@ -6,16 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter number in decimal system: ");
-        int decimalNumber = scanner.nextInt();
-        System.out.print("Enter target base: ");
-        int targetNumberBase = scanner.nextInt();
-        System.out.print("Conversion result: " + convertDecNumber(decimalNumber, targetNumberBase));
+        boolean toQuit = false;
+        String inputCommand;
+        while (!toQuit) {
+            System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            inputCommand = scanner.nextLine();
+            switch (inputCommand) {
+                case "/from":
+                    System.out.print("Enter number in decimal system: ");
+                    int decimalNumber = scanner.nextInt();
+                    System.out.print("Enter target base: ");
+                    int targetNumberBase = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Conversion result: " + convertDecNumber(decimalNumber, targetNumberBase));
+                    break;
+
+                case "/exit":
+                    toQuit = true;
+                    break;
+
+                default:
+                    System.out.println("Unknown command. Try again.");
+                    break;
+            }
+        }
     }
 
     public static String convertDecNumber(int numberToConvert, int targetNumberBase) {
         StringBuilder targetNumber = new StringBuilder();
-        if (numberToConvert == 0) {
+        if (numberToConvert == 0 || targetNumberBase == 0) {
             return "0";
         }
         while (numberToConvert > 0) {
