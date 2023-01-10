4ffa1e3b62c512728b3a3f12cc6baaae7fcf4b5d
jakubG78 <jakub.gora@wp.pl>
2023-01-10 21:56:15 +0100

    10.01.2023 commit

--- Car Sharing/Topics/What are collections/Theory/src/Task.java
+++ Car Sharing/Topics/What are collections/Theory/src/Task.java
@@ -1,7 +1,18 @@
 // You can experiment here, it won’t be checked
 
+import java.io.File;
+import java.io.IOException;
+
 public class Task {
-  public static void main(String[] args) {
-    // put your code here
+  public static void main(String[] args) throws IOException {
+    File file = new File("E:/filename.txt");
+    File renamedFile = new File("E:/newname.txt");
+
+    boolean renamed = file.renameTo(renamedFile);
+    if (renamed) {
+      System.out.println("It was successfully renamed.");
+    } else {
+      System.out.println("It was not renamed.");
+    }
   }
 }
