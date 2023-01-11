1589c01c9622fefed4a1cafda89c80adec6d9147
jakubG78 <jakub.gora@wp.pl>
2023-01-11 19:45:54 +0100

    11.01.2023 commit

--- Car Sharing/Topics/Lambda expressions/Next even number/src/Main.java
+++ Car Sharing/Topics/Lambda expressions/Next even number/src/Main.java
@@ -2,5 +2,5 @@ import java.util.function.LongUnaryOperator;
 
 class Operator {
 
-    public static LongUnaryOperator unaryOperator = // Write your code here
+    public static LongUnaryOperator unaryOperator = x -> x % 2 == 0 ? x + 2 : x + 1;
 }
