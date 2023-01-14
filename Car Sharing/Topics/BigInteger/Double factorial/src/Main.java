daf48542fb49cb992c83a1c5c48f81fa70008588
jakubG78 <jakub.gora@wp.pl>
2023-01-15 00:09:43 +0100

    15.01.2023 commit

--- Car Sharing/Topics/BigInteger/Double factorial/src/Main.java
+++ Car Sharing/Topics/BigInteger/Double factorial/src/Main.java
@@ -2,6 +2,8 @@ import java.math.BigInteger;
 
 class DoubleFactorial {
     public static BigInteger calcDoubleFactorial(int n) {
-        // type your java code here
+        long result = 0;
+        if (n == 0 || n==1) return BigInteger.ONE;
+        return BigInteger.valueOf(n).multiply(calcDoubleFactorial(n - 2));
     }
 }
