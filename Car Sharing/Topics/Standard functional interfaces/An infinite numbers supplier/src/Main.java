9619f6259ad62f6eea486d34444e470a615a5fb7
jakubG78 <jakub.gora@wp.pl>
2023-01-18 21:23:14 +0100

    18.01.2023 commit

---
+++ Car Sharing/Topics/Standard functional interfaces/An infinite numbers supplier/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.function.Supplier;
+
+class FunctionUtils {
+
+    public static Supplier<Integer> getInfiniteRange() {
+        return new Supplier<Integer>() {
+            Integer counter = 0;
+
+            @Override
+            public Integer get() {
+                return counter++;
+            }
+        };
+    }
+}
