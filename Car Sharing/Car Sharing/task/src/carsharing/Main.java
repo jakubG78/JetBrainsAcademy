679b599dd707a0a6051278de9291859863be72be
jakubG78 <jakub.gora@wp.pl>
2023-01-19 20:42:54 +0100

    19.01.2023 commit

---
+++ Car Sharing/Car Sharing/task/src/carsharing/Main.java
@@ -0,0 +1,31 @@
+package carsharing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Main {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:file:./src/carsharing/db/carsharing";
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        String table = "CREATE TABLE COMPANY" +
+                "(ID INT," +
+                "NAME VARCHAR(255))";
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL);
+            connection.setAutoCommit(true);
+            statement = connection.createStatement();
+            statement.execute("DROP TABLE IF EXISTS COMPANY");
+            statement.execute(table);
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Car Sharing/Topics/Reduction methods/Theory/src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private long balance;
+    private String number;
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
