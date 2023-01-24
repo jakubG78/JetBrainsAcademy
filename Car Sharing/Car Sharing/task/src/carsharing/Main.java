adf34b655b903b2fd1f958d6c2a12b1097288842
jakubG78 <jakub.gora@wp.pl>
2023-01-24 20:13:56 +0100

    24.01.2023 commit

--- Car Sharing/Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/Car Sharing/task/src/carsharing/Main.java
@@ -1,30 +1,30 @@
 package carsharing;
 
+import com.beust.jcommander.JCommander;
+import org.h2.jdbc.JdbcSQLSyntaxErrorException;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class Main {
     static final String JDBC_DRIVER = "org.h2.Driver";
-    static final String DB_URL = "jdbc:h2:file:./src/carsharing/db/carsharing";
 
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement statement = null;
-        String table = "CREATE TABLE COMPANY" +
-                "(ID INT PRIMARY KEY AUTO_INCREMENT," +
-                "NAME VARCHAR(255) NOT NULL UNIQUE)";
-
+    public static void main(String[] args) throws SQLException {
+        Args argv = new Args();
+        JCommander commander = JCommander.newBuilder().addObject(argv).build();
         try {
             Class.forName(JDBC_DRIVER);
-            connection = DriverManager.getConnection(DB_URL);
+            String filePath = "./src/carsharing/db/" + (argv.dbName == null ? "carsharing" : argv.dbName);
+            Connection connection = DriverManager.getConnection("jdbc:h2:file:" + filePath);
             connection.setAutoCommit(true);
-            statement = connection.createStatement();
-            statement.execute("DROP TABLE IF EXISTS COMPANY");
-            statement.execute(table);
-            statement.close();
+            CommandLineMenu.printMainMenu(connection);
             connection.close();
-        } catch (Exception e) {
+        } catch (ClassNotFoundException e) {
+            System.out.println("Wrong class");
+            e.printStackTrace();
+        } catch (JdbcSQLSyntaxErrorException e) {
+            System.out.println("Table exists");
             e.printStackTrace();
         }
     }
