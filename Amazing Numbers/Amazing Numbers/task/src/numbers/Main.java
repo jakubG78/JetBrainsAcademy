da9b4baad53f273a3bccef3418a0dfe1c108d9fa
jakubG78 <jakub.gora@gmail.com>
2023-05-31 19:59:15 +0200

    AmazingNubers stage 3/8 complete

--- Amazing Numbers/Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/Amazing Numbers/task/src/numbers/Main.java
@@ -4,32 +4,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter a natural number:");
+
         Scanner scanner = new Scanner(System.in);
-        int inputIntegerNumber = scanner.nextInt();
+        System.out.println("Welcome to Amazing Numbers!");
+        System.out.println("\nSupported requests:\n" +
+                "- enter a natural number to know its properties;\n" +
+                "- enter 0 to exit.");
         while (true) {
-            if (inputIntegerNumber <= 0) {
-                System.out.println("This number is not natural!");
+            System.out.print("\nEnter a request: > ");
+            long inputIntegerNumber = scanner.nextLong();
+            if (inputIntegerNumber == 0) {
+                System.out.println("\nGoodbye!");
                 break;
             }
-            System.out.printf("Properties of %d\n", inputIntegerNumber);
+
+            if (inputIntegerNumber <= 0) {
+                System.out.println("\nThe first parameter should be a natural number or zero.");
+                continue;
+            }
+            System.out.printf("\nProperties of %d\n", inputIntegerNumber);
             System.out.println("        even: " + isEvenNumber(inputIntegerNumber));
-            System.out.println("        odd: " + !isEvenNumber(inputIntegerNumber));
+            System.out.println("         odd: " + !isEvenNumber(inputIntegerNumber));
             System.out.println("        buzz: " + isBuzzNumber(inputIntegerNumber));
             System.out.println("        duck: " + isDuckNumber(inputIntegerNumber));
-            break;
+            System.out.println(" palindromic: " + isPalindromicNumber(inputIntegerNumber));
         }
     }
 
-    private static boolean isEvenNumber(int inputIntegerNumber) {
+    private static boolean isEvenNumber(long inputIntegerNumber) {
         return inputIntegerNumber % 2 == 0;
     }
 
-    private static boolean isBuzzNumber(int inputIntegerNumber) {
+    private static boolean isBuzzNumber(long inputIntegerNumber) {
         return inputIntegerNumber % 7 == 0 || inputIntegerNumber % 10 == 7;
     }
 
-    private static boolean isDuckNumber(int inputIntegerNumber) {
+    private static boolean isDuckNumber(long inputIntegerNumber) {
         while (inputIntegerNumber != 0) {
             if ((inputIntegerNumber % 10) == 0) {
                 return true;
@@ -37,5 +47,18 @@ public class Main {
             inputIntegerNumber = inputIntegerNumber / 10;
         }
         return false;
+    }
+
+    private static boolean isPalindromicNumber(long inputIntegerNumber) {
+        long num = inputIntegerNumber;
+        long reversedNum = 0;
+        long remainder;
+
+        while (num != 0) {
+            remainder = num % 10;
+            reversedNum = reversedNum * 10 + remainder;
+            num /= 10;
+        }
+        return inputIntegerNumber == reversedNum;
     }
 }
