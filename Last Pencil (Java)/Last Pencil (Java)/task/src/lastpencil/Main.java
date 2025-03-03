d7dc24af12194b52d65234138796efc2374a1e0c
jakubG78 <jakub.gora@gmail.com>
2025-03-03 11:35:58 +0100

    LastPencil stage 4/5 commit

--- Last Pencil (Java)/Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many pencils would you like to use:");
-        int amountOfPencils = scanner.nextInt();
+        int amountOfPencils = getAmountOfPencils(scanner);
         String fistPlayerName = "John";
         String secondPlayerName = "Jack";
         System.out.println("Who will be the first (John, Jack):");
@@ -21,6 +21,22 @@ public class Main {
                 currentPlayer = fistPlayerName;
             }
         }
+    }
+
+    private static int getAmountOfPencils(Scanner scanner) {
+        int amountOfPencils = Integer.MIN_VALUE;
+        while (amountOfPencils <= 0) {
+            try {
+                amountOfPencils = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException c) {
+                amountOfPencils = Integer.MIN_VALUE;
+            }
+            if (amountOfPencils <= 0) {
+                System.out.println(amountOfPencils == 0 ? "The number of pencils should be positive" :
+                        "The number of pencils should be numeric");
+            }
+        }
+        return amountOfPencils;
     }
 
     private static void printPencils(int amountOfPencils) {
